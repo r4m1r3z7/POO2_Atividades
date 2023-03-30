c9f83ffa1ad3921b0074032dad74b023bf09df6c
Emanuel De Araujo Ramirez <emanuel.ramirez7@gmail.com>
2023-03-30 00:23:40 -0400

    Atividade POO2
    atualizado nome do servidor SQL de acordo com o nome do sql sever de cara para corrigir warning

--- Atacado.BD.EF/Database/AtacadoContext.cs
+++ Atacado.BD.EF/Database/AtacadoContext.cs
@@ -27,8 +27,7 @@ namespace Atacado.BD.EF.Database;
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=bdAtacado;User=usrAtacado;Password=senha123;TrustServerCertificate=True;");
+                optionsBuilder.UseSqlServer("Data Source= DESKTOP-QQJDB2N;Initial Catalog=bdAtacado;User=usrAtacado;Password=senha123;TrustServerCertificate=True;");
             }
         }
 
--- AtacadoConsole/Program.cs
+++ AtacadoConsole/Program.cs
@@ -8,7 +8,7 @@ public class Program
     public static void Main(string[] args)
     {
         //Criando a string de conexão com o banco de dados SQL Server.
-        string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=bdAtacado;User=usrAtacado;Password=senha123;TrustServerCertificate=True;";
+        string connectionString = "Data Source= DESKTOP-QQJDB2N;Initial Catalog=bdAtacado;User=usrAtacado;Password=senha123;TrustServerCertificate=True;";
         var options = new DbContextOptionsBuilder<AtacadoContext>().UseSqlServer(connectionString).Options;
         var contexto = new AtacadoContext(options);
 
